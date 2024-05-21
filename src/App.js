765074c831f332f7f0d3cbea7c1955285930e166
Shubh soni <shubhsoni.dev@gmail.com>
2024-05-21 08:46:23 +0530

    enh: added bookmark design

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import "./styles.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 // const data = [
 //   {
@@ -236,6 +237,28 @@ export default function App() {
 function Feed({ newsData, setBookmark }) {
   return (
     <div>
+      <div
+        style={{
+          color: "black",
+          position: "fixed",
+          zIndex: "100000000",
+          borderRadius: "100%",
+          margin: "1rem",
+          height: "2.5rem",
+          width: "2.5rem",
+          backgroundColor: "white",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 384 512"
+          style={{ width: "1rem" }}>
+          <path d="M0 48C0 21.5 21.5 0 48 0l0 48V441.4l130.1-92.9c8.3-6 19.6-6 27.9 0L336 441.4V48H48V0H336c26.5 0 48 21.5 48 48V488c0 9-5 17.2-13 21.3s-17.6 3.4-24.9-1.8L192 397.5 37.9 507.5c-7.3 5.2-16.9 5.9-24.9 1.8S0 497 0 488V48z" />
+        </svg>
+      </div>
+
       {newsData.map((el) => (
         <div style={{ backgroundColor: "#111111", height: "100vh" }}>
           <div style={{ width: "100%", position: "relative" }}>
@@ -347,13 +370,13 @@ function Feed({ newsData, setBookmark }) {
               }}>
               Know more →
             </p>
-            {/* <div
-              style={{ padding: "1rem 2rem" }}
+            <div
+              style={{ margin: "1rem 2rem", cursor: "pointer" }}
               onClick={() => {
                 setBookmark((data) => [...data, el]);
               }}>
               ✅
-            </div> */}
+            </div>
             <p
               style={{
                 fontSize: "0.7rem",
